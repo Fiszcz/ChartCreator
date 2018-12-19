a51c5fbd6e31c37cc5392d6d3d125406db25f947
Filip Szczesniak <fiszczu@gmail.com>
2018-12-19 23:55:59 +0100

    create actions for basic settings of chart

---
+++ src/actions/basicSettingsChartActions.js
@@ -0,0 +1,28 @@
+/*
+ * action types
+ */
+
+export const SET_TITLE = 'SET_TITLE';
+export const CHANGE_TYPE_CHART = 'CHANGE_TYPE_CHART';
+export const SET_TITLE_AXIS_X = 'SET_TITLE_AXIS_X';
+export const SET_TITLE_AXIS_Y = 'SET_TITLE_AXIS_Y';
+
+/*
+ * action creators
+ */
+
+export function setTitle(title) {
+    return { type: SET_TITLE, title };
+}
+
+export function changeTypeChart(typeChart) {
+    return { type: CHANGE_TYPE_CHART, typeChart };
+}
+
+export function setTitleAxisX(title) {
+    return { type: SET_TITLE_AXIS_X, title};
+}
+
+export function setTitleAxisY(title) {
+    return { type: SET_TITLE_AXIS_Y, title};
+}
